b8ddc562697e3a8028b2ea52afda44aeacec2ed0
abr010997 <32304154+abr010997@users.noreply.github.com>
2018-10-17 23:57:30 -0600

    cambios CLIENTE
    LOS CAMBIOS DE CLIENTE
    
    APORTO LOS SP
    DELIMITER $$
    
    USE `candydecuapp`$$
    
    DROP PROCEDURE IF EXISTS `sp_cd_cliente_guardar`$$
    
    CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_cd_cliente_guardar`(IN CEDULA INT(11), IN NOM VARCHAR(50),
    IN APE1 VARCHAR(50), IN APE2 VARCHAR(50))
    BEGIN
    INSERT INTO `cd_cliente_tb` (`cd_cli_cedula`, `cd_cli_nombre`,`cd_cli_ape1`,`cd_cli_ape2`)
    VALUES (CEDULA, NOM, APE1, APE2);
    END$$
    
    DELIMITER ;
    //////////////////////////////
    /////////////////////////////
    DELIMITER $$
    
    USE `candydecuapp`$$
    
    DROP PROCEDURE IF EXISTS `sp_cd_cliente_listar`$$
    
    CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_cd_cliente_listar`()
    BEGIN
    SELECT `cd_cli_cedula`,`cd_cli_nombre`,`cd_cli_ape1`,`cd_cli_ape2`
     FROM `cd_cliente_tb`;
    END$$
    
    DELIMITER ;
    ///////////////////
    //////////////////
    DELIMITER $$
    
    USE `candydecuapp`$$
    
    DROP PROCEDURE IF EXISTS `sp_cd_cliente_buscar`$$
    
    CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_cd_cliente_buscar`(IN IDCLI INT(11))
    BEGIN
    SELECT `cd_cli_cedula`,`cd_cli_nombre`,`cd_cli_ape1`,`cd_cli_ape2`
    FROM `cd_cliente_tb`
    WHERE `cd_cli_cedula` = IDCLI;
    END$$
    
    DELIMITER ;
    /////////////////
    ////////////////
    DELIMITER $$
    
    USE `candydecuapp`$$
    
    DROP PROCEDURE IF EXISTS `sp_cd_cliente_eliminar`$$
    
    CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_cd_cliente_eliminar`(IN IDCLI INT(11))
    BEGIN
    DELETE FROM `cd_cliente_tb`
     WHERE `cd_cli_cedula` = IDCLI;
    END$$
    
    DELIMITER ;

---
+++ controller/classClienteController.php
@@ -0,0 +1,63 @@
+<?php 
+
+require_once 'model/classCliente.php';
+class classClienteController
+{
+	private $classCliente;
+	function __construct()
+	{
+		$this->classCliente = new classCliente();
+	}
+	public function index()
+	{
+		require_once 'view/header.php';
+		require_once 'view/classCliente/index.php';
+		require_once 'view/footer.php';
+	}
+	public function agregar()
+	{
+		if ($_POST) {
+			$this->classCliente->setAtributo('cd_cli_cedula',$_POST['cd_cli_cedula']);
+			$this->classCliente->setAtributo('cd_cli_nombre',$_POST['cd_cli_nombre']);
+			$this->classCliente->setAtributo('cd_cli_ape1',$_POST['cd_cli_ape1']);
+			$this->classCliente->setAtributo('cd_cli_ape2',$_POST['cd_cli_ape2']);
+			$this->classCliente->guardar();
+			header('location:?c=classCliente&m=index');
+		}
+		else{
+			require_once 'view/header.php';
+			require_once 'view/classCliente/agregar.php';
+			require_once 'view/footer.php';
+		}
+	}
+	// public function editar()
+	// {
+	// 	if ($_POST) {
+	// 		$this->classCliente->setAtributo('cd_cli_cedula',$_POST['cd_cli_cedula']);
+	// 		$this->classCliente->setAtributo('cd_cli_nombre',$_POST['cd_cli_nombre']);
+	// 		$this->classCliente->actualizar();
+	// 		header('location:?c=classCliente&m=index');
+	// 	}
+	// 	else{
+	// 		$this->classCliente = $this->classCliente->buscar($_REQUEST['id']);
+	// 		require_once 'view/header.php';
+	// 		require_once 'view/classCliente/editar.php';
+	// 		require_once 'view/footer.php';		}
+	// 	}
+
+	public function eliminar()
+	{
+		$this->classCliente->setAtributo('cd_cli_cedula',$_REQUEST['id']);
+		$this->classCliente->eliminar();
+		header('location:?c=classCliente&m=index');
+	}
+
+	public function ver()
+	{
+		$this->classCliente = $this->classCliente->buscar($_REQUEST['id']);
+		require_once 'view/header.php';
+		require_once 'view/classCliente/ver.php';
+		require_once 'view/footer.php';
+	}
+}
+?>
--- view/header.php
+++ view/header.php
@@ -45,12 +45,12 @@
         <li class="active"><a href="#">Inicio</a></li>
         <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Page 1-1</a></li>
+            <li><a href="">Page 1-1</a></li>
             <li><a href="#">Page 1-2</a></li>
             <li><a href="#">Page 1-3</a></li>
           </ul>
         </li>
-        <li><a href="#">Page 2</a></li>
+        <li><a href="?c=classCliente&m=agregar">CLIENTE</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
